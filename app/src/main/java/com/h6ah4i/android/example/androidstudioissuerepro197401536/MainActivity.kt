ae4082231e296497fd20b7d5b81db37e457aada3
Haruki Hasegawa <h6a.h4i.0@gmail.com>
2021-09-04 16:02:06 +0900

    Initial commit

---
+++ app/src/main/java/com/h6ah4i/android/example/androidstudioissuerepro197401536/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.h6ah4i.android.example.androidstudioissuerepro197401536
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
